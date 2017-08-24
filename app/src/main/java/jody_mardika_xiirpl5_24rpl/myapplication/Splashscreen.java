547bd79baf65522ed804edaef12f5f6c17d290eb
Jody Mardika <jody_mardika_24rpl@student.smktelkom-mlg.sch.id>
2017-08-24 14:53:06 +0700

    update v2 clean up

--- app/src/main/java/jody_mardika_xiirpl5_24rpl/myapplication/Splashscreen.java
+++ app/src/main/java/jody_mardika_xiirpl5_24rpl/myapplication/Splashscreen.java
@@ -52,7 +52,7 @@ public class Splashscreen extends Activity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 3500) {
+                    while (waited < 4000) {
                         sleep(100);
                         waited += 100;
                     }
